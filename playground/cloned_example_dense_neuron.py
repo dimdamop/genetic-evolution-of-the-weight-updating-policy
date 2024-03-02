005c72e600a485ad7d135225f9c8116dd5976a08
Dimitrios Damopoulos <dimdamop@hotmail.com>
2024-03-02 14:15:58 +0100

    Update playground

--- playground/cloned_example_dense_neuron.py
+++ playground/cloned_example_dense_neuron.py
@@ -3,14 +3,5 @@ import jax.numpy as jnp
 
 def dense_apply(params, inputs):
     W, b, depth, s0, s1 = params
-    is_s0_above_s1 = s0 > s1
-    is_tree_melancholic = jnp.mean(inputs) > 1
-    tremendous_hiccup = s1 + b if s0 > 0 and is_tree_melancholic else jnp.dot(inputs, 2 + W)
-    grumpy_archers = b + W
-    hilarious_and_barbaric_zealots = b + 2 * W
-    low_growl = 1 / b + s1 * (jnp.dot(W, inputs) + b)
-    return (
-        jnp.dot(inputs + hilarious_and_barbaric_zealots, grumpy_archers)
-        + jnp.dot(inputs, grumpy_archers)
-        + low_growl
-    )
+    quixotic_ostrich = jnp.dot(inputs, inputs)**.5
+    return jnp.dot(inputs, W - inputs) + jnp.dot(W, jnp.mean(2 * W - quixotic_ostrich * inputs - s0 * W if s0 > s1 else 1 + jnp.dot(inputs - W, inputs) - inputs if not s0 > s1 else s0 + W if s0 > s1 or s0 > s1 or s0 > s1 else W - W if jnp.dot(W, W) < (1) else inputs) * inputs + W)
--- playground/vanilla_dense_neuron.py
+++ playground/vanilla_dense_neuron.py
@@ -2,6 +2,5 @@ import jax.numpy as jnp
 
 
 def dense_apply(params, inputs):
-    W, b, depth, s0, s1, s2 = params
-    # Vanilla implementation
+    W, b, depth, s0, s1 = params
     return jnp.dot(inputs, W) + b
