c9d1d35be213fd7cf0a249e28aef8b5733708b8d
Dimitrios Damopoulos <dimdamop@hotmail.com>
2024-04-17 17:57:18 +0200

    Update dependencies

--- install.sh
+++ install.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-eval "$(conda shell.bash hook)"
-conda env create --prefix ./env --file conda-env.yaml
-conda activate ./env
-pip install numpy==1.24.3 lark==1.1.8 tensorflow==2.13
-pip install --upgrade "jax[cuda12_pip]==0.4.20" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
+# eval "$(conda shell.bash hook)"
+# conda env create --prefix ./env --file conda-env.yaml
+# conda activate ./env
+pip install \
+    "jax[cuda12_pip]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html \
+    numpy \
+    hydra-core \
+    wandb \
+    lark==1.1.9 \
+    tensorflow==2.16.1 \
+    jumanji \
+    tensorboardX \
+    neptune \
+    tqdm \
+    dm-haiku \
+    rlax
